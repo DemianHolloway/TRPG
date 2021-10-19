ff5514b6dc05a03036a3ba04b16480648622b19a
DemianHolloway <82787192+DemianHolloway@users.noreply.github.com>
2021-10-19 17:35:23 +0900

    Add files via upload

---
+++ newdiscord.py
@@ -0,0 +1,47 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.uesr.id)
+    print("ready")
+    game = discord.Game("테스트봇")
+    await  client.change_presence(status=discord.Status.online, activity=game)
+
+@client.event
+async def on_member_join(member):
+    role = ""
+    for i in member.server.roles:
+        role = i
+        break
+        await client.add_roles(member, role)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+
+        await message.channel.send("응")
+
+    if message.content.startswith("/사진"):
+        pic = message.content.split(" ")[1]
+        await message.channel.send(file=discord.File(pic))
+
+    if message.content.startswith("/채널메시지"):
+        channel = message.content[7:25]
+        msg = message.content[26:]
+        await client.get_channel(int(channel)).send(msg)
+
+    if message.content.startswith(":Dm"):
+        author = message.guild.get_member(int(message.content[4:22]))
+        msg = message.content[23:]
+        await author.send(msg)
+
+    if message.content.startswith("내정보"):
+
+        await message.channel.send("응")
+
+
+client.run("ODkzMzgzNjgxODM2NzI0Mjc1.YVaqZA.izDvnzfyaI3wap9poGX1ROSJ5RQ")
+
+
