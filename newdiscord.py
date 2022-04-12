96aedaba6149c8299fb7f50924b3e2731dc82ef6
DemianHolloway <82787192+DemianHolloway@users.noreply.github.com>
2022-04-12 15:51:49 +0900

    Update newdiscord.py

--- newdiscord.py
+++ newdiscord.py
@@ -2,7 +2,55 @@ import discord
 import datetime
 import os
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 client = discord.Client()
+
+scope = [
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/drive',
+]
+json_file_name = '{credentials.json}'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+gc = gspread.authorize(credentials)
+spreadsheet_url = '{https://docs.google.com/spreadsheets/d/1UUxldG6brJ87g-WJiPGlMtJQQWY59uh3C2GNXwGH9JI/edit?usp=sharing}'
+
+
+# 스프레스시트 문서 가져오기
+doc = gc.open_by_url(spreadsheet_url)
+
+if message.content.startswith("!목록"):
+# 시트 선택하기
+worksheet = doc.worksheet('db')
+range_list = worksheet.range('{"A1:C5"}')
+    for cell in range_list:
+        values[i].append(cell.value)
+        break
+    await message.channel.send(embed=embed)
+
+"""worksheet.update_acell('B1', 'b1 updated')
+# 특정 셀 쓰기
+
+worksheet.append_row(['new1', 'new2', 'new3', 'new4'])
+# 행 추가1
+
+worksheet.insert_row(['new1', 'new2', 'new3', 'new4'], 4)
+# 행 추가2"""
+
+
+"""worksheet.resize(10,4)
+# 크기 조정
+
+gs = gc.create('{스프레드 시트명}')
+# 스프레드시트 추가
+
+worksheet = gs.add_worksheet(title='{시트명}', rows='{행 수}', cols='{열 수}')
+# 시트 추가
+
+gs.share('{이메일 주소}', perm_type='user', role='{권한}')
+# 계정 공유"""
+
 
 @client.event
 async def on_ready():
